74c5a2b7acbd6d55099fc51740d80b7d1c92461e
serchg0george <serchg0george@gmail.com>
2024-11-06 09:51:10 +0200

    Initial commit

---
+++ src/main/java/com/is/countryneighborstour/CountryNeighborsTourApplication.java
@@ -0,0 +1,13 @@
+package com.is.countryneighborstour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryNeighborsTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryNeighborsTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/is/countryneighborstour/CountryNeighborsTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.countryneighborstour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryNeighborsTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
