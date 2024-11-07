1c2b18eae3b97323e8a3405cdb50eed6245bd034
serchg0george <serchg0george@gmail.com>
2024-11-07 20:54:33 +0200

    Implemented custom handling of BadRequest exception

--- src/main/java/com/is/countryneighborstour/services/impl/CountriesServiceImpl.java
+++ src/main/java/com/is/countryneighborstour/services/impl/CountriesServiceImpl.java
@@ -1,32 +1,35 @@
 package com.is.countryneighborstour.services.impl;
 
 import com.is.countryneighborstour.dto.CountryInfoDto;
+import com.is.countryneighborstour.exceptions.TripCountryBadRequestException;
 import com.is.countryneighborstour.services.CountriesService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 /**
  * Service implementation for retrieving country information, including border details, based on country codes.
- *
+ * <p>
  * This service uses a WebClient to fetch country data from the REST Countries API
  * (https://restcountries.com/v3.1/name/). The service can retrieve information for a specified country
  * and returns it as a stream of {@link CountryInfoDto} objects.
- *
  **/
 
 @Service
-@PropertySource("classpath:application.properties")
+@RequiredArgsConstructor
 public class CountriesServiceImpl implements CountriesService {
 
     @Value("${rest-countries.api.address}")
     private String restCountriesApiAddress;
 
-    private final WebClient webClient;
+    private WebClient webClient;
 
-    public CountriesServiceImpl(@Value("${rest-countries.api.address}") String restCountriesApiAddress) {
-        this.restCountriesApiAddress = restCountriesApiAddress;
+    @PostConstruct
+    public void init() {
         this.webClient = WebClient.create(restCountriesApiAddress);
     }
 
@@ -35,6 +38,8 @@ public class CountriesServiceImpl implements CountriesService {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.path(countryCode).build())
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> response.bodyToMono(String.class)
+                        .flatMap(errorBody -> Mono.error(new TripCountryBadRequestException(countryCode))))
                 .bodyToFlux(CountryInfoDto.class).blockFirst();
     }
 }
--- src/main/java/com/is/countryneighborstour/services/impl/CurrencyServiceImpl.java
+++ src/main/java/com/is/countryneighborstour/services/impl/CurrencyServiceImpl.java
@@ -1,23 +1,27 @@
 package com.is.countryneighborstour.services.impl;
 
 import com.is.countryneighborstour.dto.RatesDto;
+import com.is.countryneighborstour.exceptions.CurrencyBadRequestException;
 import com.is.countryneighborstour.services.CurrencyService;
 import com.is.countryneighborstour.services.ExchangeRateService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Service implementation for handling currency exchange rate data.
- *
+ * <p>
  * This service uses a WebClient to fetch the latest exchange rates from an external API
  * (http://data.fixer.io/api/latest). The service can retrieve all exchange rates based on
  * a specified base currency and convert them to a Flux of {@link RatesDto}.
- *
  **/
 
 @Service
+@RequiredArgsConstructor
 public class CurrencyServiceImpl implements CurrencyService {
 
     @Value("${fixer.api.address}")
@@ -26,15 +30,13 @@ public class CurrencyServiceImpl implements CurrencyService {
     @Value("${fixer.api.key}")
     private String fixerApiKey;
 
-    private final WebClient webClient;
+    private WebClient webClient;
 
     ExchangeRateService exchangeRateService;
 
-    public CurrencyServiceImpl(@Value("${fixer.api.address}") String fixerApiAddress, @Value("${fixer.api.key}") String fixerApiKey, ExchangeRateService exchangeRateService) {
-        this.fixerApiKey = fixerApiKey;
-        this.fixerApiAddress = fixerApiAddress;
+    @PostConstruct
+    public void init() {
         this.webClient = WebClient.create(fixerApiAddress);
-        this.exchangeRateService = exchangeRateService;
     }
 
     @Override
@@ -45,9 +47,13 @@ public class CurrencyServiceImpl implements CurrencyService {
                         .build())
                 .retrieve()
                 .bodyToFlux(RatesDto.class)
-                .doOnNext(ratesDto ->
-                    exchangeRateService.saveExchangeRates(baseCurrency, ratesDto.getRates())
-                );
+                .flatMap(ratesDto -> {
+                    if (ratesDto.getRates() == null || ratesDto.getRates().isEmpty()) {
+                        return Mono.error(new CurrencyBadRequestException(baseCurrency));
+                    }
+                    return Mono.just(ratesDto);
+                })
+                .doOnNext(ratesDto -> exchangeRateService.saveExchangeRates(baseCurrency, ratesDto.getRates()));
     }
 
 }
