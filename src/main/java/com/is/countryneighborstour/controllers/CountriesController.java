5b96b585a4e33a87adf421042fc7bfaedc5e27b1
serchg0george <serchg0george@gmail.com>
2024-11-07 21:39:49 +0200

    Refactored using ResponseEntity

--- src/main/java/com/is/countryneighborstour/controllers/CountriesController.java
+++ src/main/java/com/is/countryneighborstour/controllers/CountriesController.java
@@ -3,6 +3,7 @@ package com.is.countryneighborstour.controllers;
 import com.is.countryneighborstour.dto.CountryInfoDto;
 import com.is.countryneighborstour.services.CountriesService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class CountriesController {
     private final CountriesService countriesService;
 
     @GetMapping("/get-country/{countryCode}")
-    public CountryInfoDto getAllCountries(@PathVariable String countryCode) {
-        return countriesService.getAllBorders(countryCode);
+    public ResponseEntity<CountryInfoDto> getAllCountries(@PathVariable String countryCode) {
+        return ResponseEntity.ok(countriesService.getAllBorders(countryCode));
     }
 }
--- src/main/java/com/is/countryneighborstour/controllers/CurrenciesController.java
+++ src/main/java/com/is/countryneighborstour/controllers/CurrenciesController.java
@@ -3,6 +3,7 @@ package com.is.countryneighborstour.controllers;
 import com.is.countryneighborstour.dto.RatesDto;
 import com.is.countryneighborstour.services.CurrencyService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class CurrenciesController {
     private final CurrencyService currencyService;
 
     @GetMapping("/get-rates/{baseCurrency}")
-    public Flux<RatesDto> getRates(@PathVariable String baseCurrency) {
-        return currencyService.getAllRates(baseCurrency);
+    public ResponseEntity<Flux<RatesDto>> getRates(@PathVariable String baseCurrency) {
+        return ResponseEntity.ok(currencyService.getAllRates(baseCurrency));
     }
 }
