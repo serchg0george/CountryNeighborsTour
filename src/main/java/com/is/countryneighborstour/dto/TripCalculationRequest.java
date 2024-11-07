389f27a1bde83ad3a3f7b35932111a01d1c5d4a2
serchg0george <serchg0george@gmail.com>
2024-11-07 22:03:28 +0200

    Refactored using wrapper Integer

--- src/main/java/com/is/countryneighborstour/dto/TripCalculationRequest.java
+++ src/main/java/com/is/countryneighborstour/dto/TripCalculationRequest.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 @Getter
 public class TripCalculationRequest {
     private String country;
-    private int totalBudget;
-    private int budgetPerCountry;
+    private Integer totalBudget;
+    private Integer budgetPerCountry;
     private String currency;
 }
