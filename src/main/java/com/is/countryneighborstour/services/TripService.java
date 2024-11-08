243f8c3a0fbb42f3b266e68fc4cd115c947834cb
serchg0george <serchg0george@gmail.com>
2024-11-08 11:15:02 +0200

    Refactored method calculatePriceForCountry is divided into smaller methods

--- src/main/java/com/is/countryneighborstour/services/TripService.java
+++ src/main/java/com/is/countryneighborstour/services/TripService.java
@@ -1,9 +1,21 @@
 package com.is.countryneighborstour.services;
 
+import com.is.countryneighborstour.dto.CountryBudgetDto;
 import com.is.countryneighborstour.dto.TripCalculationResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface TripService {
     TripCalculationResponse calculatePriceForCountry(String country, Integer totalBudget, Integer budgetPerCountry, String currency);
+
+    List<String> getBorderCountries(String country);
+
+    List<CountryBudgetDto> calculateCountryBudgets(List<String> borders, Integer budgetPerCountry, String baseCurrency);
+
+    BigDecimal calculateNeededLocalCurrency(BigDecimal rateValue, Integer budgetPerCountry);
+
+    Integer[] calculateTripNumbers(Integer totalBudget, Integer budgetPerCountry, Integer neighborCount);
 
     String findLocalCurrency(String country);
 }
--- src/main/java/com/is/countryneighborstour/services/impl/TripServiceImpl.java
+++ src/main/java/com/is/countryneighborstour/services/impl/TripServiceImpl.java
@@ -1,5 +1,6 @@
 package com.is.countryneighborstour.services.impl;
 
+import com.is.countryneighborstour.dto.CountryBudgetDto;
 import com.is.countryneighborstour.dto.CountryInfoDto;
 import com.is.countryneighborstour.dto.TripCalculationResponse;
 import com.is.countryneighborstour.entities.ExchangeRates;
@@ -13,9 +14,12 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+/**
+ * Service implementation for handling trip calculations, including budget allocation per country,
+ * determining the number of trips possible within a given budget, and currency conversions.
+ */
 
 @Service
 @AllArgsConstructor
@@ -26,31 +30,51 @@ public class TripServiceImpl implements TripService {
 
     @Override
     public TripCalculationResponse calculatePriceForCountry(String country, Integer totalBudget, Integer budgetPerCountry, String baseCurrency) {
+        List<String> borders = getBorderCountries(country);
 
+        List<CountryBudgetDto> countryBudgets = calculateCountryBudgets(borders, budgetPerCountry, baseCurrency);
+
+        Integer[] frequencyData = calculateTripNumbers(totalBudget, budgetPerCountry, borders.size());
+        Integer timesToVisit = frequencyData[0];
+        Integer remainingBudget = frequencyData[1];
+
+        return new TripCalculationResponse(timesToVisit, remainingBudget, baseCurrency, countryBudgets);
+    }
+
+    @Override
+    public List<String> getBorderCountries(String country) {
         CountryInfoDto countryInfo = countriesService.getAllBorders(country);
-        List<String> borders = countryInfo.getBorders();
+        return countryInfo.getBorders();
+    }
 
-        Map<String, BigDecimal> currenciesMap = new HashMap<>();
-
-        List<TripCalculationResponse.CountryBudget> countryBudgets = new ArrayList<>();
+    @Override
+    public List<CountryBudgetDto> calculateCountryBudgets(List<String> borders, Integer budgetPerCountry, String baseCurrency) {
+        List<CountryBudgetDto> countryBudgets = new ArrayList<>();
 
         for (String border : borders) {
             String localCurrency = findLocalCurrency(border);
             ExchangeRates rate = exchangeRateRepository.findByBaseCurrencyAndTargetCurrencyAndDate(baseCurrency, localCurrency, LocalDate.now());
 
             if (rate != null) {
-                BigDecimal rateValue = rate.getRate();
-                BigDecimal neededLocalCurrency = BigDecimal.valueOf(rateValue.longValue() * budgetPerCountry).setScale(2, RoundingMode.HALF_UP);
-                currenciesMap.put(localCurrency, neededLocalCurrency);
-                countryBudgets.add(new TripCalculationResponse.CountryBudget(border, localCurrency, neededLocalCurrency));
+                BigDecimal neededLocalCurrency = calculateNeededLocalCurrency(rate.getRate(), budgetPerCountry);
+                countryBudgets.add(new CountryBudgetDto(border, localCurrency, neededLocalCurrency));
             }
         }
 
-        Integer neighborCount = borders.size();
-        Integer timesToVisit = totalBudget / (neighborCount * budgetPerCountry);
-        Integer remainingBudget = totalBudget % (neighborCount * budgetPerCountry);
+        return countryBudgets;
+    }
 
-        return new TripCalculationResponse(timesToVisit, remainingBudget, baseCurrency, countryBudgets);
+    @Override
+    public BigDecimal calculateNeededLocalCurrency(BigDecimal rateValue, Integer budgetPerCountry) {
+        return rateValue.multiply(BigDecimal.valueOf(budgetPerCountry)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public Integer[] calculateTripNumbers(Integer totalBudget, Integer budgetPerCountry, Integer neighborCount) {
+        Integer totalTripCost = neighborCount * budgetPerCountry;
+        Integer timesToVisit = totalBudget / totalTripCost;
+        Integer remainingBudget = totalBudget % totalTripCost;
+        return new Integer[] {timesToVisit, remainingBudget};
     }
 
     @Override
