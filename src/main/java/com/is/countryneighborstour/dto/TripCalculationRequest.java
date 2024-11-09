999b0b2553a99c05d3fe20a92d6243f85d7829a5
serchg0george <serchg0george@gmail.com>
2024-11-09 11:02:19 +0200

    Refactored using records

--- src/main/java/com/is/countryneighborstour/dto/TripCalculationRequest.java
+++ src/main/java/com/is/countryneighborstour/dto/TripCalculationRequest.java
@@ -3,27 +3,17 @@ package com.is.countryneighborstour.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
 /**
  * This class contains details such as the destination country, the total budget, the
  * budget per country, and the currency in which the budget is represented.
  */
 
-@Getter
-public class TripCalculationRequest {
+public record TripCalculationRequest(@NotBlank @Size(min = 2, max = 3) String country,
 
-    @NotBlank
-    @Size(min = 2, max = 3)
-    private String country;
+                                     @NotNull Integer totalBudget,
 
-    @NotNull
-    private Integer totalBudget;
+                                     @NotNull Integer budgetPerCountry,
 
-    @NotNull
-    private Integer budgetPerCountry;
-
-    @NotBlank
-    @Size(min = 3, max = 3)
-    private String currency;
+                                     @NotBlank @Size(min = 3, max = 3) String currency) {
 }
