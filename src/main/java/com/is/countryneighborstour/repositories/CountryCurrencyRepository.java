d798be59349da63ff45f8432fbc03b0b119cd97b
serchg0george <serchg0george@gmail.com>
2024-11-09 14:49:26 +0200

    Refactored using another search parameter for repository

--- src/main/java/com/is/countryneighborstour/repositories/CountryCurrencyRepository.java
+++ src/main/java/com/is/countryneighborstour/repositories/CountryCurrencyRepository.java
@@ -4,7 +4,10 @@ import com.is.countryneighborstour.entities.CountryCurrency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface CountryCurrencyRepository extends JpaRepository<CountryCurrency, Long> {
-    CountryCurrency findByCountryCode(String countryCode);
+    List<CountryCurrency> findByCountryCodeIn(List<String> countryCodes);
 }
